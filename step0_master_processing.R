d85f1c010ce9166843822d08af3c719732cc2a84
Gerard Cramer (work) <gerard@dairycowfootdoc.com>
2026-06-05 11:58:24 -0500

    fix accidental delete

--- step0_master_processing.R
+++ step0_master_processing.R
@@ -3,7 +3,7 @@ source('functions/setup_default_processing_options.R') #default settings: 1 exam
 
 #**** Modify This Section*** turn on to over ride default processing options
 
-#clean_up_old_files <- FALSE # Use FALSE here for your own data, or after the first download of example data. This will delete any previously processed files as well as raw data in the event_files folder
+clean_up_old_files <- FALSE # Use FALSE here for your own data, or after the first download of example data. This will delete any previously processed files as well as raw data in the event_files folder
 
 get_EXAMPLE_herds <- 0 # (0-8) ## number of Parnell Example herds you want to process.## if this is set to 0, you need to put your own data in the event_files folder
 
